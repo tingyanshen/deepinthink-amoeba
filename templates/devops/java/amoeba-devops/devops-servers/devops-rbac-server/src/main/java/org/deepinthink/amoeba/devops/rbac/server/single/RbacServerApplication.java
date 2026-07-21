ee6695bae69c83bec95a9aa37a5825a2ffc676c7
Tingyan Shen <tingyan.shen@gmail.com>
2026-07-21 14:03:35 +0900

    GH-39 Initialized adding spring boot rbac service (#40)

---
+++ templates/devops/java/amoeba-devops/devops-servers/devops-rbac-server/src/main/java/org/deepinthink/amoeba/devops/rbac/server/single/RbacServerApplication.java
@@ -0,0 +1,11 @@
+package org.deepinthink.amoeba.devops.rbac.server.single;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbacServerApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(RbacServerApplication.class, args);
+  }
+}
